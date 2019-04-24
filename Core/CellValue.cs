4bc0f1f0c1a90892b3c66b7a5fc08461e4fb484c
Руслан Горлатий <Hrat1995@gmail.com>
2019-04-25 01:39:00 +0300

    Add CellValue class

---
+++ Core/CellValue.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Common;
+
+namespace Core
+{
+    public class CellValue : Cell
+    {
+        public CellValue(int row, int column, int value) : base(row, column, value) { }        
+    }
+}
