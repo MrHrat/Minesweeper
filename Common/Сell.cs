e803587e841efaae3daf52726c39b7c1897fb2c7
Руслан Горлатий <Hrat1995@gmail.com>
2019-04-24 17:22:13 +0300

    Add class Cell

---
+++ Common/Сell.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Common
+{
+    public class Сell
+    {
+        public int Row
+        {
+            get;
+            private set;
+        }
+
+        public int Column
+        {
+            get;
+            private set;
+        }
+
+        public Сell(int row, int colum)
+        {
+            Row = row;
+            Column = colum;
+        }
+    }
+}
