b1dae34d0fb168eae0f9200b6e7fb0a514d8ff88
Руслан Горлатий <Hrat1995@gmail.com>
2019-04-25 01:27:48 +0300

    Add TestAlgorithm class. singleton patern

---
+++ Algorithm/TestAlgorithm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Common;
+
+namespace Algorithm
+{
+    public class TestAlgorithm
+    {
+        private TestAlgorithm() { }
+
+        private static TestAlgorithm _testAlgorithmInstance = null;
+        public static TestAlgorithm GetInstance()
+        {
+            if (_testAlgorithmInstance == null)
+            {
+                _testAlgorithmInstance = new TestAlgorithm();
+            }
+            return _testAlgorithmInstance;
+        }
+
+        public static Cell GetСhoice()
+        {
+            var TestAlgorithm = GetInstance();
+
+            return new Cell(1,1);
+        }
+    }
+}
