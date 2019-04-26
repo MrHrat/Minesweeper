a3ea24e98f652d5a1a9312b3cd81ab5ea4842edc
Руслан Горлатий <Hrat1995@gmail.com>
2019-04-26 13:59:45 +0300

    Add a constructor to the class CellValue

--- Core/CellValue.cs
+++ Core/CellValue.cs
@@ -5,6 +5,7 @@ namespace Core
 {
     public class CellValue : Cell
     {
-        public CellValue(int row, int column, int value) : base(row, column, value) { }        
+        public CellValue(int row, int column, int value) : base(row, column, value) { }
+        public CellValue(Cell cell, int value) : base(cell.Row, cell.Column, value) { }
     }
 }
