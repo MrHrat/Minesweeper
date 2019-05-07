dc79e4a9d5ff0c304b1745c2b94ff483076e6db8
Руслан Горлатий <Hrat1995@gmail.com>
2019-05-07 06:43:01 +0300

    TestAlgorithm write algorithm

--- Algorithm/TestAlgorithm.cs
+++ Algorithm/TestAlgorithm.cs
@@ -8,6 +8,9 @@ namespace Algorithm
     public class TestAlgorithm
     {
         private int _size;
+        private int _countMine;
+        private ListСell _listСell { set; get; }
+        private ListСell _markCells { set; get; }
 
         private TestAlgorithm() { }
 
@@ -21,27 +24,155 @@ namespace Algorithm
             return _testAlgorithmInstance;
         }
 
-        public static Cell GetСhoice(List<Cell> list, int size)
+        public static Cell GetСhoice(ListСell listСell, ListСell markCells, int size, int countMine)
         {
             var TestAlgorithm = GetInstance();
             TestAlgorithm._size = size;
-
-            if((list != null) && (!list.Any()))
+            TestAlgorithm._countMine = countMine;
+            TestAlgorithm._listСell = listСell;
+            TestAlgorithm._markCells = markCells;
+            
+            if ((listСell.Count == 0) || (TestAlgorithm.GetMark() == null))
             {
-                return TestAlgorithm.RandomSelection();
+                return Cell.Random(TestAlgorithm._size);
             }
-
-            return new Cell(1, 1);
+            else
+            {
+                return TestAlgorithm.GetMark();                
+            }
         }
 
-        public Cell RandomSelection()
+        public Cell GetMark()
         {
+            foreach (Cell cell in _listСell)
+            {
+                if (cell.Value != 0)
+                {
+                    var aroundCells = _listСell.GetAroundCellsNoTags(cell, _size);
+
+                    if (cell.Value == aroundCells.Count)
+                    {
+                        foreach (Cell field in aroundCells)
+                        {
+                            if (!_markCells.IsPresent(field))
+                            {
+                                return new Cell(field.Row, field.Column, true);
+                            }
+                        }
+                    }
+                }
+            }
+
+            return Get100Choice();
+        }
+
+        public Cell Get100Choice()
+        {
+            foreach (Cell cell in _listСell)
+            {
+                if (cell.Value != 0)
+                {
+                    var aroundCells = _listСell.GetAroundCellsNoTags(cell, _size);
+
+                    if (cell.Value != aroundCells.Count)
+                    {
+                        var countFlags = 0;
+
+                        foreach (Cell field in aroundCells)
+                        {
+                            if (_markCells.IsPresent(field))
+                            {
+                                countFlags++;
+                            }
+                        }
+
+                        if (cell.Value == countFlags)
+                        {
+                            foreach (Cell field in aroundCells)
+                            {
+                                if (!_markCells.IsPresent(field))
+                                {
+                                    return field;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            return GetBestRandomChoice();
+        }
+
+        public Cell GetBestRandomChoice()
+        {
+            var listNoTags = ListСell.GetReverseCells(_size, _listСell, _markCells);
+
+            var minPercent = 10.0;
+            var totalPercent = 1.0 * (_countMine - _markCells.Count) / (_size * _size - _listСell.Count);
+            var randomList = new ListСell();
+
+            foreach (Cell field in listNoTags)
+            {
+                var numberCells = listNoTags.GetAroundCellsNoTags(field, _size);
+                var maxPercentCell = GetMaxPercentCell(numberCells);
+                AddRandomList(randomList, field, maxPercentCell, minPercent, totalPercent);
+            }
+
             Random rand = new Random();
+            return randomList[rand.Next(randomList.Count)];
+        }
 
-            var row = rand.Next(_size) + 1;
-            var column = rand.Next(_size) + 1;
+        void AddRandomList(ListСell cells, Cell field, double maxPercentCell, double minPercent, double totalPercent)
+        {
+            if ((maxPercentCell < minPercent) && (maxPercentCell != 0))
+            {
+                minPercent = maxPercentCell;
+                cells.Clear();
+            }
 
-            return new Cell(row, column);
+            if (totalPercent < minPercent)
+            {
+                minPercent = totalPercent;
+                cells.Clear();
+            }
+
+            if ((maxPercentCell == minPercent) || (totalPercent == minPercent))
+            {
+                cells.Add(field);
+            }
+        }
+
+        double GetMaxPercentCell(ListСell cells)
+        {
+            var maxPercentCell = 0.0;
+
+            foreach (Cell numberField in cells)
+            {
+                if (_listСell.IsPresent(numberField))
+                {
+                    var aroundCells = _listСell.GetAroundCellsNoTags(numberField, _size);
+                    var countFlags = 0;
+
+                    foreach (Cell aroundField in aroundCells)
+                    {
+                        if (_markCells.IsPresent(aroundField))
+                        {
+                            countFlags++;
+                        }
+                    }
+
+                    var countNoFlags = aroundCells.Count - countFlags;
+
+                    var PercentCell = 1.0 * (_listСell[numberField.Row, numberField.Column].Value - countFlags) / countNoFlags;
+
+                    if (maxPercentCell < PercentCell)
+                    {
+                        maxPercentCell = PercentCell;
+                    }
+                }
+            }
+
+            return maxPercentCell;
         }
     }
 }
