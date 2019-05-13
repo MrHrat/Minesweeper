ec545e8524819c466c9cdc8ecf2015196f9413c5
Руслан Горлатий <Hrat1995@gmail.com>
2019-05-13 13:26:04 +0300

    Finish WpfUI

---
+++ WpfUI/WpfUI/MainViewModel.cs
@@ -0,0 +1,135 @@
+﻿using Common;
+using Core;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace WpfUI
+{
+    internal class MainViewModel : INotifyPropertyChanged
+    {
+        public GameField gameField;
+        
+        public int RowCount
+        {
+            get
+            {
+                return gameField.Size.Row;
+            }
+        }
+        public int ColumnCount
+        {
+            get
+            {
+                return gameField.Size.Column;
+            }
+        }
+
+        const int gridSize = 10;
+
+        private ObservableCollection<CellsGrid> cells = new ObservableCollection<CellsGrid>();
+        public ObservableCollection<CellsGrid> Cells
+        {
+            get { return cells; }
+            set
+            {
+                cells = value;
+                OnPropertyChanged("Cells");
+            }
+        }
+                
+        public MainViewModel()
+        {
+            gameField = new GameField(10, 10, 10);
+            gameField.GenerateMines();
+            cells = new ObservableCollection<CellsGrid>();
+            Fill();
+        }
+
+        public void Fill()
+        {
+            Cells.Clear();
+            for (var row = 0; row < gameField.Size.Row; row++)
+            {
+                for (var column = 0; column < gameField.Size.Column; column++)
+                {
+                    Cells.Add(new CellsGrid(gameField[row, column]));                    
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+
+    class CellsGrid
+    {
+        public Сell Tag { set; get; }
+        public object Content { set; get; }
+        public SolidColorBrush Background { set; get; }
+        static BrushConverter bc = new BrushConverter();
+
+        public CellsGrid(Сell tag)
+        {
+            Tag = tag;
+            Content = setContent(tag);
+        }
+
+        private object setContent(Сell cell)
+        {
+            switch (cell.Status)
+            {
+                case CellStatus.Explosion:
+                    Background = (SolidColorBrush)bc.ConvertFrom("#e50000");
+                    return GetObjImage(@"..\..\Explosion.png");
+                case CellStatus.Mark:
+                    return GetObjImage(@"..\..\Flag.png");
+                case CellStatus.MarkMine:
+                    Background = (SolidColorBrush)bc.ConvertFrom("#58e669");
+                    return GetObjImage(@"..\..\BombMark.png");
+                case CellStatus.Mine:
+                    return GetObjImage(@"..\..\Bomb.png");
+                case CellStatus.Number:
+                    Background = (SolidColorBrush)bc.ConvertFrom("#FFFACD");
+                    return cell.Value;
+                case CellStatus.Open:
+                    Background = (SolidColorBrush)bc.ConvertFrom("#FFFACD");
+                    return string.Empty;
+                default:
+                    Background = (SolidColorBrush)bc.ConvertFrom("#58e669");
+                    return string.Empty;
+            }
+        }
+
+        object GetObjImage(string url)
+        {
+            StackPanel sp = new StackPanel();
+            sp.Orientation = Orientation.Horizontal;
+            Image flagImage = new Image();
+            BitmapImage bi = new BitmapImage();
+            bi.BeginInit();
+            bi.UriSource = new Uri(url, UriKind.Relative);
+            bi.EndInit();
+            flagImage.Source = bi;
+            sp.Children.Add(flagImage);
+
+            return sp;
+        }
+
+        public override string ToString()
+        {
+            return Content.ToString();
+        }
+    }
+}
--- WpfUI/WpfUI/MainWindow.xaml.cs
+++ WpfUI/WpfUI/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,23 +16,67 @@ using System.Windows.Shapes;
 using Algorithm;
 using Common;
 using Core;
-
+using MahApps.Metro.Controls;
 
 namespace WpfUI
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
+        private MainViewModel viewModel;
+
         public MainWindow()
         {
+            viewModel = new MainViewModel();
+            DataContext = viewModel;
             InitializeComponent();
+        }
+
+        private void Button_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var btn = (Button)sender;
+            var cell = (Сell)btn.Tag;
+            var mark = false;
+            Сell value;
+
+            if (e.ChangedButton == MouseButton.Right) mark = true;
+            if (e.ChangedButton == MouseButton.Middle)
+            {
+                value = TestAlgorithm.GetСhoice(viewModel.gameField.VisibleСells, viewModel.gameField.Marks,
+                    viewModel.gameField.Size, viewModel.gameField.CountMines);
+            }
+            else
+            {                
+                value = new Сell(cell.Row, cell.Column, mark);
+            }
+            viewModel.gameField.OpenCell(value);
+            if (viewModel.gameField.Status == GameStatus.Play)
+            {
+                lblChose.Text = value.ToString();
+            }
+            lblStatus.Text = viewModel.gameField.Status.ToString();
+            viewModel.Fill();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            viewModel = new MainViewModel();
+            DataContext = viewModel;
+        }
 
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Сell value = TestAlgorithm.GetСhoice(viewModel.gameField.VisibleСells, viewModel.gameField.Marks,
+                    viewModel.gameField.Size, viewModel.gameField.CountMines);
+            viewModel.gameField.OpenCell(value);
+            if (viewModel.gameField.Status == GameStatus.Play)
+            {
+                lblChose.Text = value.ToString();
+            }
+            lblStatus.Text = viewModel.gameField.Status.ToString();
+            viewModel.Fill();
         }
     }
 }
